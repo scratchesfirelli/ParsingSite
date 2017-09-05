0df18e8f0ea791e2deb8782d6ef747969e9915c8
Aleksey Tarasov <aleksey.tarasov@ukad-group.com>
2017-09-05 14:06:21 +0300

    Renamed file

--- SbResultsParsing/Startup.cs
+++ SbResultsParsing/Startup.cs
@@ -16,8 +16,8 @@ namespace SbResultsParsing
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddJsonFile("project.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"project.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
